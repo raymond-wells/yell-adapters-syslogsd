988158db90d4397e1ed81b351d1a044ae7aad0d7
Raymond F. Wells <rfw2nd@gmail.com>
2014-07-07 12:47:11 -0700

    Make logger resilient to variable arguments.

--- lib/yell/adapters/syslogsd.rb
+++ lib/yell/adapters/syslogsd.rb
@@ -17,13 +17,17 @@ module Yell
         messages.select{|m| m.is_a? Hash}.reduce(&:merge) || {}
       end
 
+      def get_combined_message *messages
+        messages.select{|m| m.is_a? String}.join(' ') || ""
+      end
+      
       def get_notify_for event, message, data
         data.merge({:short_message => "#{@prefix}#{message}", :level => event.level, :facility => @facility})
       end
       
       write do |event|
         structured_data = yank_hashes_from(*event.messages)
-        message = format(*event.messages)
+        message = get_combined_message(*event.messages)
         @instance.notify! get_notify_for(event, message, structured_data)
       end
     end
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -13,6 +13,21 @@ describe Yell::Adapters::Syslogsd do
     @logger = Yell.new(:syslogsd, {:facility => "rspec"}) 
   end
 
+  it "should handle more than 2 arguments" do
+    @logger.info "Hello", "there", "this", "is", "a", "test", {:struct => true}
+    expect($_mock_data[0][:short_message]).to eq "Hello there this is a test"
+    expect($_mock_data[0][:struct]).to eq true
+  end
+  
+  it "should handle no arguments" do
+    @logger.info 
+  end
+  
+  it "should handle multi-line messages" do
+    @logger.info "Test\nAnd another"
+    expect($_mock_data[0][:short_message]).to eq "Test\nAnd another"
+  end
+  
   it "convert input into the format expected by syslog-sd" do
     @logger.info "Test", {:with_structured => 1}
     expect($_mock_data).to eq [{:with_structured => 1, :facility => "rspec", :level => 1, :short_message => "Test"}]
